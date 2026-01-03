db06fe928ff7cd2b08dd57ce11807ddcf955d56a
Darshan <midoria646@gmail.com>
2026-01-03 13:42:56 +0530

    Completed user login mechanics

--- Backend/logic.py
+++ Backend/logic.py
@@ -1,7 +1,41 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func
-from models import User, Trip, TripStop, Activity,TripActivity,Budget
+from models import User, Trip, TripStop, Activity,TripActivity,Budget,User
 from datetime import datetime   
+import auth
+
+def register_user(db: Session, data: dict):
+    existing_user = db.query(User).filter(User.email == data['email']).first()
+    if existing_user:
+        return {"status":"error", "message":"Email already registered"}
+    
+    hashed_password = auth.get_password_hash(data['password'])
+
+    new_user = User(
+        name = data['name'],
+        email = data['email'],
+        password_hash = hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)    
+
+    return new_user
+
+def login_user(db: Session, data: dict):
+    email = data['email']
+    password = data['password']
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user :
+        return {"status":"error", "message":"User not found"}
+    
+    if not auth.verify_password(password, user.password_hash):
+        return {"status":"error", "message":"Incorrect password"}   
+    
+    return {"status":"success", "user_id": user.id, "name": user.name}
 
 def create_trip(db: Session, data: dict) :
     new_trip = Trip(
