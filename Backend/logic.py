001769af5246ff2df30c3c9e16e508c4a24ed3b6
Darshan <midoria646@gmail.com>
2026-01-03 11:37:43 +0530

    Added TripStop function

--- Backend/logic.py
+++ Backend/logic.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from models import User, Trip
+from sqlalchemy import func
+from models import User, Trip, TripStop
 from datetime import datetime   
 
 def create_trip(db: Session, data: dict) :
@@ -13,3 +14,21 @@ def create_trip(db: Session, data: dict) :
     db.commit()
     db.refresh(new_trip)
     return new_trip
+
+def add_new_stop(db : Session, data: dict ):
+
+    highest_stop = db.query(func.max(TripStop.stop_order)).filter(TripStop.trip_id == data['trip_id']).scalar()
+    
+    new_stop = TripStop(
+        trip_id = data['trip_id'],
+        city = data['city'],
+        start_date = datetime.strptime(data['start_date'], '%Y-%m-%d').date(),
+        end_date = datetime.strptime(data['end_date'], '%Y-%m-%d').date(),
+        stop_order = highest_stop + 1 if highest_stop != None else 1
+    )
+
+    db.add(new_stop)
+    db.commit()
+    db.refresh(new_stop)
+
+    return new_stop
