78ff9474ac78e968cf6e29966ef38676bedd1a83
Darshan <midoria646@gmail.com>
2026-01-03 12:11:05 +0530

    Added add new activit function

--- Backend/logic.py
+++ Backend/logic.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func
-from models import User, Trip, TripStop
+from models import User, Trip, TripStop, Activity,TripActivity
 from datetime import datetime   
 
 def create_trip(db: Session, data: dict) :
@@ -32,3 +32,27 @@ def add_new_stop(db : Session, data: dict ):
     db.refresh(new_stop)
 
     return new_stop
+
+def add_new_activity(db: Session, data: dict):
+    new_activity = Activity(
+        name = data['name'],
+        description = data.get('description', ''),
+        city = data.get('city', ''),
+        cost = data.get('cost', 0.0),
+        duration_hours = data.get('duration_hours', 0)
+    )
+
+    db.add(new_activity)
+    db.flush()
+
+    new_link = TripActivity(
+        trip_stop_id = data['trip_stop_id'],
+        activity_id = new_activity.id,
+        scheduled_time = datetime.strptime(data['scheduled_time'], '%Y-%m-%d %H:%M:%S') if 'scheduled_time' in data else datetime.now()
+    )
+
+    db.add(new_link)
+    db.commit()
+    db.refresh(new_activity)
+
+    return new_activity
